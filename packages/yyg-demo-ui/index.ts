2f65a7851f298f974b18c6854c4c41fe6c94a33f
zhangyoubin <zhangyoubin@winchannel.net>
2023-03-08 17:18:13 +0800

    cli 完成

--- packages/yyg-demo-ui/index.ts
+++ packages/yyg-demo-ui/index.ts
@@ -1,9 +1,12 @@
 import { App } from 'vue'
 import Foo from '@yyg-demo-ui/foo'
+import YggDemoUi from '@yyg-demo-ui/ygg-demo-ui'
 // import component end
 import '../scss/index.scss'
 
-const components = [Foo] // components
+const components = [Foo,
+  YggDemoUi
+] // components
 
 // 全局动态添加组件
 const install = (app: App): void => {
